a69b472f504ffd2f6999f2d757e1cc959e458957
Aman kankriya <bantukankriya@gmail.com>
2025-10-19 21:43:48 +0530

    changes to test

--- src/data_validation.py
+++ src/data_validation.py
@@ -3,7 +3,7 @@ import pandas as pd
 def validate_data(path="data/iris.csv"):
     df = pd.read_csv(path)
     assert not df.isnull().values.any(), "Data contains nulls!"
-    assert df.shape[1] == 4, "Unexpected number of columns!"
+    assert df.shape[1] == 5, "Unexpected number of columns!"
     return True
 
 if __name__ == "__main__":
